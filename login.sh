6a946925375c59250b31305d0b917999c1c18ed6
Ratakk <124754724+Ratakk@users.noreply.github.com>
2024-04-20 16:15:11 -0500

    Beto

--- login.sh
+++ login.sh
@@ -3,11 +3,11 @@
 bash banner.sh
 echo
 
-read -p $'\e[1;32m  Enter \033[33mUsername \033[37mfor \033[32mLogin:\e[0m ' username                
-read -p $'\e[1;32m  Enter \033[33mPassword \033[37mfor \033[32mLogin:\e[0m ' password 
+read -p $'\e[1;32m  Escribe tu \033[33mNombre \033[37mpara \033[32mentrar:\e[0m ' Nombre                
+read -p $'\e[1;32m  Escribe tu \033[33mcontra \033[37mpara \033[32mentrar:\e[0m ' Contra 
 echo
 echo
-read -p $'\033[1m\033[32m  Your \033[0mShell \033[38;5;209mName\033[31m: \033[33m\033[1m ' names
+read -p $'\033[1m\033[32m  Tu \033[0mnick \033[38;5;209mname\033[31m: \033[33m\033[1m ' alias
 cd                                                   
 cd ..                                               
 cd usr/etc                                       
@@ -29,19 +29,19 @@ echo -e "\e[1;32m
                                                            
 
 \033[31m           ────────────────────────────
-\033[33m               Login To \033[32mContinue
+\033[33m               Inicia sesion \033[32mahora
 \033[31m           ────────────────────────────
 
 
 \e[0m"
 echo
-read -p $'       \e[33m\033[1m\033[33m[\033[31m+\033[33m] \033[37mINPUT \033[33mUSERNAME FOR LOGIN:\033[32m ' user
-read -s -p $'       \e[32m\033[1m\033[33m[\033[31m+\033[33m] \033[37mINPUT \033[33mPASSWORD FOR LOGIN:\033[33m ' pass                                                
-if [[ \$pass == $password && \$user == $username ]]; then
+read -p $'       \e[33m\033[1m\033[33m[\033[31m+\033[33m] \033[37mIngresa \033[33mNombre para entrar:\033[32m ' nombre
+read -s -p $'       \e[32m\033[1m\033[33m[\033[31m+\033[33m] \033[37mIngresa \033[33mContra para entrar:\033[33m ' contra                                                
+if [[ \$contra == $Contra && \$nombre == $Nombre ]]; then
 sleep 3
 clear
 cd $HOME
-cd TermuX-Custom
+cd ConTermux
 cd Song
 python sound_effect.py
 clear
@@ -49,19 +49,19 @@ cd $HOME
 echo -e "\033[1m\033[33m
 
 
-██╗  ██╗ █████╗  ██████╗██╗  ██╗███████╗██████╗ 
-██║  ██║██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗
-███████║███████║██║     █████╔╝ █████╗  ██████╔╝
-██╔══██║██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗
-██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║
-╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                                                                    
+██████╗░███████╗████████╗░█████╗░
+██╔══██╗██╔════╝╚══██╔══╝██╔══██╗
+██████╦╝█████╗░░░░░██║░░░██║░░██║
+██╔══██╗██╔══╝░░░░░██║░░░██║░░██║
+██████╦╝███████╗░░░██║░░░╚█████╔╝
+╚═════╝░╚══════╝░░░╚═╝░░░░╚════╝░                                                                    
 
 "
-echo -e  "     \e[1m\e[32m▂▃▄▅▆▇▓▒░ \033[1mCoded By \e[33mCyberAK \e[1m\e[32m░▒▓▇▆▅▄▃▂"
+echo -e  "     \e[1m\e[32m▂▃▄▅▆▇▓▒░ \033[1mCoded By \e[33mBeto \e[1m\e[32m░▒▓▇▆▅▄▃▂"
 cd $HOME
 echo -e "   \033[1m\033[33m]\033[31m──────────────────────────────────────\033[33m["
 echo 
-PS1='\033[1m\[\e[32m\]\033[1m┌─[\[\e[37m\]\T\[\e[32m\]\033[1m]─────\033[1m\e[1;98m\[[\033[1m\033[37m$names\033[32m]\033[1m\e[0;32m\033[1m───[\033[38;5;209m\#\033[32m]\n|\n\033[1m\e[0;32m\033[1m└─[\[\e[32m\]\e[1;33m\W\[\e[1m\033[32m]\033[1m────►\e[1;36m\033[1m '
+PS1='\033[1m\[\e[32m\]\033[1m┌─[\[\e[37m\]\T\[\e[32m\]\033[1m]─────\033[1m\e[1;98m\[[\033[1m\033[37m$alias\033[32m]\033[1m\e[0;32m\033[1m───[\033[38;5;209m\#\033[32m]\n|\n\033[1m\e[0;32m\033[1m└─[\[\e[32m\]\e[1;33m\W\[\e[1m\033[32m]\033[1m────►\e[1;36m\033[1m '
 << comment
 shopt -s autocd
 shopt -s cdspell
@@ -81,11 +81,11 @@ shopt -s compat32
 shopt -s lithist
 comment
 cd $HOME
-cd TermuX-Custom
+cd ConTermux
 cd
 else
 echo ""
-echo -e "\e[1;31m  You Entered wrong Details! 
+echo -e "\e[1;31m  ¡Ingresó detalles incorrectos! 
 \e[0m"
 sleep 1
 cmatrix -L
@@ -95,7 +95,7 @@ LOGIN
 echo 
 echo
 echo 
-echo -e "\033[1m\e[1;32m Your Termux is \033[33mReady \n
-       So please \033[31mExit \033[37mand \033[32mLogin.\e[0m"
+echo -e "\033[1m\e[1;32m Tu Termux esta \033[33mListo \n
+       Porfavor \033[31msal \033[37my \033[32mentra.\e[0m"
 echo
 echo
