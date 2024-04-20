3340f4e87b88a08cf95f3d42ae19ea5f79242fc8
Ratakk <124754724+Ratakk@users.noreply.github.com>
2024-04-20 15:57:04 -0500

    Beto

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 # Function to update the repository
 update_repository() {
-    echo "Updating repository..."
+    echo "Actualizando el repositorio..."
     bash update.sh
-    echo "Repository updated successfully!"
+    echo "¡Repositorio actualizado con éxito!"
 }
 
 # Main script
@@ -12,7 +12,7 @@ clear
 echo
 pkg install pv -y >/dev/null 2>&1
 echo -e "\033[32m\033[1m{───────────────────────────────────────────────────}"
-echo -e "\033[33m\033[1m   Installing All Required Packages! Please Wait..." | pv -qL 10
+echo -e "\033[33m\033[1m   ¡Instalando todos los paquetes necesarios!" | pv -qL 10
 apt update                    
 apt upgrade -y 
 pkg install python -y 
@@ -26,20 +26,20 @@ pip install random
 pip install requests 
 pkg install python2 -y 
 pkg install termux-api -y 
-echo -e "\033[31m\033[1m        INSTALLATION COMPLETED \033[32m[\033[36m✓\033[32m]" | pv -qL 12
+echo -e "\033[31m\033[1m        INSTALACIÓN COMPLETADA \033[32m[\033[36m✓\033[32m]" | pv -qL 12
 echo -e "\033[33m\033[1m]────────────────────────────────────────────["
 termux-setup-storage
-cd $HOME/TermuX-Custom
+cd $HOME/ConTermux
 cp login.sh $PREFIX/etc
 chmod +x login.sh delete.sh setup.sh sound_effect.py banner.sh update.sh
-mkdir -p $HOME/TermuX-Custom/Song 
-mv Access-Granted.mp3 Jarvis2.mp3 JARVIS.mp3 sound_effect.py $HOME/TermuX-Custom/Song
+mkdir -p $HOME/ConTermux/Song 
+mv Acceso-concedido.mp3 Sonido2.mp3 Sonido1.mp3 sound_effect.py $HOME/ConTermux/Song
 rm 1
-mkdir -p $HOME/TermuX-Custom/NETWORK
-mv network.py $HOME/TermuX-Custom/NETWORK
+mkdir -p $HOME/ConTermux/NETWORK
+mv network.py $HOME/ConTermux/NETWORK
 
 # Prompt user for update
-read -p "Do you want to update the repository now? (y/n): " choice
+read -p "¿Quieres actualizar el repositorio ahora? (y/n): " choice
 if [ "$choice" = "y" ]; then
     update_repository
 fi
