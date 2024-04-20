504b816002aa3ef0f6d0962e3978dc3696716d52
Ratakk <124754724+Ratakk@users.noreply.github.com>
2024-04-20 15:50:32 -0500

    Beto

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Navigate to the TermuX-Custom directory
-cd $HOME/TermuX-Custom || exit
+cd $HOME/ConTermux || exit
 
 # Pull the latest changes from the remote repository
 git pull origin main
 
 # Provide feedback to the user
-echo "Repository updated successfully!"
+echo "¡Repositorio actualizado con éxito!"
